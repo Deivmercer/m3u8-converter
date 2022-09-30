6761bd7d77368eb5a49510fc7d38e208db55f080
Davide Costantini <davemercerdave@gmail.com>
2022-09-30 20:47:20 +0200

    Fixed replace already existing file

--- m3u8 converter.bat
+++ m3u8 converter.bat
@@ -1,3 +1,8 @@
+@REM m3u8 converter
+@REM Usage: "m3u8 converter.bat"
+@REM m3u8 files must be located in .\m3u8
+@REM Author: Davide Costantini
+
 @echo off
 
 if not exist "%~dp0m3u8\" (
@@ -17,11 +22,12 @@ for /r ".\m3u8" %%f in (*.m3u8) do (
     echo %~dp0mp4\%%~nf.mp4
     if exist "%~dp0mp4\%%~nf.mp4" (
         set /p choice="File with name "%%~nf.mp4" already exists. Would you like to replace it? [y/n] "
-        if "%choice%" equ "y" del "%~dp0mp4\%%~nf.mp4"
+        if "%choice%" equ "y" (
+            del "%~dp0mp4\%%~nf.mp4"
+        )
     )
     start /d %vlc_path% /w vlc.exe "%%f" -I dummy --sout="#transcode{vcodec=h264,acodec=mpga,ab=128,channels=2,samplerate=44100,scodec=none,deinterlace}:std{access=file{no-overwrite},mux=mp4,dst='%~dp0mp4\%%~nf.mp4'}" vlc://quit
 	move "%%f" "%~dp0\converted\"
-    pause
 )
 
 :end
